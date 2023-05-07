bf9611a77e2090983cdd2d0f66cf1e2794304aa3
lestece <>
2023-05-07 13:25:11 +0000

    Create functional component and start working on stateless with props

---
+++ my-app/src/components/FunctionalGreetings.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+// - WITH VANILLA JAVASCRIPT
+// function FunctionalGreeting() {
+//     return <h1>Hello from React!</h1>
+// }
+
+// - WITH ARROW FUNCTION:
+const FunctionalGreeting = () => <h1>Hello from React!</h1>;
+
+export default FunctionalGreeting;
