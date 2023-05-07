42e8a13e8d4df802285136230b3244ce651de229
lestece <>
2023-05-07 16:37:16 +0000

    Add props, retrieve and render

--- my-app/src/components/FunctionalGreetingsWithProps.js
+++ my-app/src/components/FunctionalGreetingsWithProps.js
@@ -6,6 +6,9 @@ import React from "react";
 // }
 
 // - WITH ARROW FUNCTION:
-const FunctionalGreetingWithProps = () => <h1>Hello!</h1>;
+const FunctionalGreetingWithProps = (props) => {
+console.log(props);
+return <h1>Hello, {props.name}! I see you're {props.age} years old; {props.greeting}! </h1>;
+}
 
 export default FunctionalGreetingWithProps;
