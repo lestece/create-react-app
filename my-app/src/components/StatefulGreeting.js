9ad651c4d8626ff8ba56a273611d7ae722d8998e
lestece <>
2023-05-08 15:20:05 +0000

    Update the state upon user interaction

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -8,12 +8,19 @@ class StatefulGreeting extends React.Component{
             buttonText: "Exit",
         };
     }
+
+    handleClick(){
+        this.setState({
+            introduction: "Goodbye",
+            buttonText: "Enter",
+        });
+    }
     
     render() {
         return (
             <div>
                 <h1>{this.state.introduction}, {this.props.name}!; {this.props.greeting}</h1> 
-                <button>{this.state.buttonText}</button>
+                <button onClick={()=>this.handleClick()}>{this.state.buttonText}</button>
             </div>
         ) 
     }
