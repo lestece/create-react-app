5001c3c5e89735c08ea15c40d2249e000bc632eb
lestece <>
2023-05-09 16:49:49 +0000

    third form of the setState; correctly update/toggle prevState

--- my-app/src/components/StatefulGreetingWithPrevState.js
+++ my-app/src/components/StatefulGreetingWithPrevState.js
@@ -11,31 +11,48 @@ class StatefulGreetingWithPrevState extends React.Component {
     }
 
     handleClick() {
-        this.setState({
-            introduction: this.state.introduction === "Hello" ? "Goodbye" : "Hello",
-            buttonText: this.state.buttonText === "Exit" ? "Enter" : "Exit",
-        }, () => {
-            console.log('new state',this.state.introduction);
-            console.log('new state',this.state.buttonText);
-        }
-        );
-        console.log(this.state.introduction);
-        console.log(this.state.buttonText);
-    }
-
-    incrementCount(){
-        this.setState({
-            count: this.state.count + 1,
+        // Wrong way of updating the state to the previous one
+        // this.setState({
+        //     introduction: this.state.introduction === "Hello" ? "Goodbye" : "Hello",
+        //     buttonText: this.state.buttonText === "Exit" ? "Enter" : "Exit",
+        // }, () => {
+        //     console.log('new state',this.state.introduction);
+        //     console.log('new state',this.state.buttonText);
+        // }
+        // );
+        // console.log(this.state.introduction);
+        // console.log(this.state.buttonText);
+        this.setState((prevState, prevProps)=>{
+            console.log('Previous state:', prevState)
+            console.log('Previous props:', prevProps)
+            return{
+                introduction: prevState.introduction === "Hello" ? "Goodbye" : "Hello",
+                buttonText: prevState.buttonText === "Exit" ? "Enter" : "Exit"
+            }
         })
     }
 
-    incrementByFive(){
-        this.incrementCount()
-        this.incrementCount()
-        this.incrementCount()
-        this.incrementCount()
-        this.incrementCount()
+    incrementCount(){
+        // Wrong way of updating the state to the previous one
+        // this.setState({
+        //     count: this.state.count + 1,
+        // })
+        this.setState((prevState, prevProps) => {
+            console.log('Previous state:', prevState)
+            console.log('Previous props:', prevProps)
+            return{
+                count: prevState.count + 1
+            }
+        })
     }
+
+    // incrementByFive(){
+    //     this.incrementCount()
+    //     this.incrementCount()
+    //     this.incrementCount()
+    //     this.incrementCount()
+    //     this.incrementCount()
+    // }
 
     render() {
         return (
@@ -44,8 +61,8 @@ class StatefulGreetingWithPrevState extends React.Component {
                 <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
                 <button onClick={() => this.incrementCount()}>Increment</button>
                 <p>You've clicked {this.state.count} times!</p>
-                <button onClick={() => this.incrementByFive()}>5 increment</button>
-                <p>Increment by 5: {this.state.count}</p>
+                {/* <button onClick={() => this.incrementByFive()}>5 increment</button>
+                <p>Increment by 5: {this.state.count}</p> */}
             </div>
         )
     }
