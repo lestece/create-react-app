7602139a74c7f664b99f3e2c9aa00a73a710d53f
lestece <>
2023-05-11 18:14:20 +0000

    Install ES7 snippets and try out some snippets in ES7Snippets.js file

--- README.md
+++
@@ -1,2 +0,0 @@
-# create-react-app
-How to create a React app - CI tutorial
---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,21 @@
+// Snippet 1
+// import React from 'react'
+
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+// Snippet 2
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
